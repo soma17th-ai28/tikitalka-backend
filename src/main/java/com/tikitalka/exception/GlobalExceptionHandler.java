2b902eaa26e2eb85b0c3b0b6d84479dd0b1edf4f
huro0906 <huro0906@gmail.com>
2026-05-09 13:35:04 +0900

    [Feat] GlobalExceptionHandler 구현

--- src/main/java/com/tikitalka/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tikitalka/exception/GlobalExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.tikitalka.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<?> handleIOException(IOException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("error", "Google Sheets API Error", "message", e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneralException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("error", "Internal Server Error", "message", e.getMessage()));
+    }
 }
